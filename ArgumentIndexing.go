58e752facf8d98d03873244fad3c52bc7f164349
Nabin Gautam <Nabingautam384994@gmail.com>
2025-03-15 10:23:21 +0545

    added comment

--- ArgumentIndexing.go
+++ ArgumentIndexing.go
@@ -11,6 +11,7 @@ func main() {
 		Pass   bool   = true
 	)
 
+	//Argument indexing is done with %[index]v
 	fmt.Printf("Student %v has scored %v in course %v at the small age of %v. But the question still remainson if he has passed? %v. He has passed which is obvious and achieved such feat at the age of %[4]v",
 		Name, Grade, Course, Age, Pass)
 }
