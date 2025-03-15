42da28b86ddef8fa8075ecbd265eebdc4b238b30
Nabin Gautam <Nabingautam384994@gmail.com>
2025-03-15 15:19:31 +0545

    Type

--- ArgumentIndexing.go
+++ ArgumentIndexing.go
@@ -12,6 +12,10 @@ func main() {
 	)
 
 	//Argument indexing is done with %[index]v
-	fmt.Printf("Student %v has scored %v in course %v at the small age of %v. But the question still remainson if he has passed? %v. He has passed which is obvious and achieved such feat at the age of %[4]v",
+	fmt.Printf("Student %v has scored %v in course %v at the small age of %v. But the question still remainson if he has passed? %v. He has passed which is obvious and achieved such feat at the age of %[4]v\n",
+		Name, Grade, Course, Age, Pass)
+
+	//Added Type Safety
+	fmt.Printf("Student %s has scored %s in course %s at the small age of %d. But the question still remainson if he has passed? %t. He has passed which is obvious and achieved such feat at the age of %[4]v",
 		Name, Grade, Course, Age, Pass)
 }
