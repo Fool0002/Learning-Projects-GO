c30b03b97ff361bc7aa8e72c10d8595ed0057938
Nabin Gautam <Nabingautam384994@gmail.com>
2025-03-15 10:21:52 +0545

    arhument INdexing try

---
+++ ArgumentIndexing.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		Name   string = "Ramu"
+		Age    int    = 18
+		Course string = "Science"
+		Grade  string = "A+"
+		Pass   bool   = true
+	)
+
+	fmt.Printf("Student %v has scored %v in course %v at the small age of %v. But the question still remainson if he has passed? %v. He has passed which is obvious and achieved such feat at the age of %[4]v",
+		Name, Grade, Course, Age, Pass)
+}
